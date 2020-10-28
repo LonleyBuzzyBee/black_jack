aa879a8be6eac4e216f9e1cac5737bc5c4716102
LonleyBuzzyBee <57302847+LonleyBuzzyBee@users.noreply.github.com>
2020-10-27 18:01:57 -0700

    debugging cardScore function

--- ex.py
+++ ex.py
@@ -1,12 +1,14 @@
 from random import randint
 
+# this function generates a random card between 1 and 13
 def getCard():
   card = randint(1, 13)
   return card
 
-def displayCard():
-  card = getCard()
+# this function assigns which card you drew out of the card deck as a string
+def displayCard(card):
   cards = ("Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King")
+
   suit_generator = randint(0, 4)
   if suit_generator == 0:
     suit = "Spades"
@@ -20,13 +22,21 @@ def displayCard():
   your_card = cards[card - 1]
   return your_card + " of " + suit
 
+# this function gets the card value
+def cardScore(card):
+  if card >= 2 or card <= 10:
+    card_score = card
+  elif card == 1:
+    card_score = 11
+  elif card > 10:
+    card_score = 10
   
-# def cardScore():
+  return card_score
+  
 # def dealCard():
 # def deal():
 # def playHand():
 # def results():
-
 
 
 print("Welcome to Mai & Lana's Blackjack Game! 🐥")
@@ -34,7 +44,34 @@ print("Welcome to Mai & Lana's Blackjack Game! 🐥")
 playing_game = input("Would you like to play black jack? enter Y or N: ")
 
 while playing_game.upper() == "Y":
-  print("here is your card: ", displayCard())
+
+  player_score = 0 
+  dealer_score = 0
+
+  # player first card
+  player_card = getCard()
+  print("PLAYER was dealt: ", displayCard(player_card))
+  player_score += cardScore(player_card)
+
+  # dealer first card
+  dealer_card = getCard()
+  print("DEALER was dealt: ", displayCard(dealer_card))
+  dealer_score += cardScore(dealer_card)
+
+  # player second card
+  player_card = getCard()
+  print("PLAYER was dealt: ", displayCard(player_card))
+  player_score += cardScore(player_card)
+
+  # dealer second card
+  dealer_card = getCard()
+  print("DEALER was dealt: ", displayCard(dealer_card))
+  dealer_score += cardScore(dealer_card)
+
+  print(f"PLAYER score = {player_score}")
+  print(f"DEALER score = {dealer_score}")
+
+
   playing_game = input("Would you like to play again? enter Y or N: ")
 
 print("Okay, thanks for playing. See ya later!")
